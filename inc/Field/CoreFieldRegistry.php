aedce236b863d40064ed9be1cc6234ec7e52a7b3
Casey Milne <cjmofficio@gmail.com>
2025-06-02 20:25:33 -0700

    Update core field type register.

--- inc/Field/CoreFieldRegistry.php
+++ inc/Field/CoreFieldRegistry.php
@@ -5,8 +5,9 @@ namespace F4\Field;
 class CoreFieldRegistry {
 
     public static function register() {
-        FieldRegistry::register('text', \F4\Field\TextField::class);
-        FieldRegistry::register('number', \F4\Field\NumberField::class);
-        FieldRegistry::register('hidden', \F4\Field\HiddenField::class);
+        FieldRegistry::register('text', \F4\Field\FieldType\TextField::class);
+        FieldRegistry::register('number', \F4\Field\FieldType\NumberField::class);
+        FieldRegistry::register('hidden', \F4\Field\FieldType\HiddenField::class);
+        FieldRegistry::register('select', \F4\Field\FieldType\SelectField::class);
     }
 }
